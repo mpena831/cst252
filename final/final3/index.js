1cede7cda080b4fbfc7ff89f344be0f3c33c78ab
gall2060 <58701826+gall2060@users.noreply.github.com>
2020-05-01 16:11:02 -0700

    change
    change

--- final/final3/index.js
+++ final/final3/index.js
@@ -1,18 +1,41 @@
-var summary = {
+// var summary = {
+//   "url": "https://api.covid19api.com/summary",
+//   "method": "GET",
+//   "data": { }
+//   "timeout": 0,
+// };
+
+// var countries = $("#list");
+// $.each(summary, function(val, text){
+//   countires.append(
+//     $('<option></option>').val(val).html(text)
+//     );
+// });
+
+// $.ajax(countries).done(function (response) {
+//   console.log(response);
+//   $("#output").html("<p>" + )
+// });
+
+  var settings = {
   "url": "https://api.covid19api.com/summary",
   "method": "GET",
-  "data": { }
   "timeout": 0,
 };
 
-var countries = $("#list");
-$.each(summary, function(val, text){
-  countires.append(
-    $('<option></option>').val(val).html(text)
-    );
-});
-
-$.ajax(countries).done(function (response) {
+$.ajax(settings).done(function (response) {
   console.log(response);
-  $("#output").html("<p>" + )
+  var nop = response.Countries[0];
+  console.log(nop);
+
+    var tCases = response.Global.TotalConfirmed.toLocaleString();
+    var nConfirmed = response.Global.NewConfirmed.toLocaleString();
+    var tDeaths = response.Global.TotalDeaths.toLocaleString();
+    var tRecovered = response.Global.TotalRecovered.toLocaleString();
+
+    $("#cases").html(tCases);
+    $("#deaths").html(tDeaths);
+    $("#recovered").html(tRecovered);
+    $("#new").html(nConfirmed);
+
 });
