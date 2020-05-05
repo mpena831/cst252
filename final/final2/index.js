c4691ab936adf0e78d374b7809a911f788493c64
gall2060 <58701826+gall2060@users.noreply.github.com>
2020-05-05 16:49:09 -0700

    change
    c

--- final/final2/index.js
+++ final/final2/index.js
@@ -1,25 +1,92 @@
-//   var settings = {
-//   "url": "https://api.covid19api.com/summary",
-//   "method": "GET",
-//   "timeout": 0,
-//   "data": { }
-// };
+  var settings = {
+  "url": "https://api.covid19api.com/summary",
+  "method": "GET",
+  "timeout": 0,
+  "data": { }
+};
 
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-//   // var nop = response.Countries[0];
-//   // console.log(nop);
+$.ajax(settings).done(function (data) {
+  // console.log(response);
 
-//     var tCases = response.Global.TotalConfirmed.toLocaleString();
-//     var nConfirmed = response.Global.NewConfirmed.toLocaleString();
-//     var tDeaths = response.Global.TotalDeaths.toLocaleString();
-//     var tRecovered = response.Global.TotalRecovered.toLocaleString();
+  //   var tCases = response.Global.TotalConfirmed.toLocaleString();
+  //   var nConfirmed = response.Global.NewConfirmed.toLocaleString();
+  //   var tDeaths = response.Global.TotalDeaths.toLocaleString();
+  //   var tRecovered = response.Global.TotalRecovered.toLocaleString();
 
-//     // $("#cases").html(tCases);
-//     // $("#deaths").html(tDeaths);
-//     // $("#recovered").html(tRecovered);
-//     // $("#new").html(nConfirmed);
-// });
+  //   $("#cases").html(tCases);
+  //   $("#deaths").html(tDeaths);
+  //   $("#recovered").html(tRecovered);
+  //   $("#new").html(nConfirmed);
+  var globalObj = data.Global;
+    console.log("New Confirmed Cases", globalObj.NewConfirmed);
+    // api format:
+    // [
+    //  {
+    //    Country: "ALA Aland Islands",
+    //    CountryCode: "AX",
+    //    Slug: "ala-aland-islands",
+    //    NewConfirmed: 0,
+    //    TotalConfirmed: 0,
+    //    NewDeaths: 0,
+    //    TotalDeaths: 0,
+    //    NewRecovered: 0,
+    //    TotalRecovered: 0,
+    //    Date: "2020-05-04T18:06:50Z"
+    //  },
+    // ]
+    // svgmap format: 
+    // AF: {
+    //  TotalConfirmed: 0,
+    //  NewDeaths: 0,
+    //  TotalDeaths: 0,
+    //  NewRecovered: 0,
+    //  TotalRecovered: 0
+    // },
+    // parse country data and construct structure for svgmap
+    var countryObjArray = data.Countries;
+    console.log("First country object:", countryObjArray[0]);
+    console.log("First country:", countryObjArray[0].Country);
+    var mapCountryObj = {};
+
+    for (i=0; i < countryObjArray.length; i++) {
+      // use country code as key to map obj
+      var thisKey = countryObjArray[i].CountryCode;
+
+      mapCountryObj[thisKey].TotalConfirmed = countryObjArray[i].TotalConfirmed;
+      mapCountryObj[thisKey].TotalDeaths = countryObjArray[i].TotalDeaths;
+      mapCountryObj[thisKey].TotalRecovered = countryObjArray[i].TotalRecovered;
+    }
+    // generate map w svgmap
+    // new svgMap({
+    //   targetElementID: 'svgMap',
+    //   data: {
+    //     data: {
+    //       gdp: {
+    //         name: 'GDP per capita',
+    //         format: '{0} USD',
+    //         thousandSeparator: ',',
+    //         thresholdMax: 50000,
+    //         thresholdMin: 1000
+    //       },
+    //       change: {
+    //         name: 'Change to year before',
+    //         format: '{0} %'
+    //       }
+    //     },
+    //     applyData: 'gdp',
+    //     values: {
+    //       AF: {gdp: 587, change: 4.73},
+    //       AL: {gdp: 4583, change: 11.09},
+    //       DZ: {gdp: 4293, change: 10.01}
+    //       // ...
+    //     }
+    //  } 
+    // });
+    //
+    // values: mapCountryObj;
+    // render map
+
+});
 
 new svgMap({
 
@@ -45,11 +112,7 @@ new svgMap({
     },
     applyData: 'cases',
     values: {
-      AF: {cases: 7000, deaths: 12 , recovered: 222},
-      AL: {cases: 7000, deaths: 12 , recovered: 222},
-      DZ: {cases: 7000, deaths: 12, recovered: 222},
-      CN: {cases: 123, deaths: 12, recovered: 222},
-      MX: {cases: 7000, deaths: 12, recovered: 222}
+      AF: {cases: 3, deaths:  3, recovered: 3},
       // ...
     }
   }
