cb79f4a5c5a21a3e342e185c575a3a823871582e
mpena831 <64433701+mpena831@users.noreply.github.com>
2020-05-05 17:10:45 -0700

    marco changes

--- final/final2/index.js
+++ final/final2/index.js
@@ -1,28 +1,48 @@
-//   var settings = {
-//   "url": "https://api.covid19api.com/summary",
-//   "method": "GET",
-//   "timeout": 0,
-//   "data": { }
-// };
+$.ajax() {
+  url: "https://api.covid19api.com/summary",
+  type: "GET",
+  success: function(data){
+    // write global data to global div
+    var globalObj = data.Global;
+    console.log("New Confirmed Cases", globalObj.NewConfirmed);
 
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-//   // var nop = response.Countries[0];
-//   // console.log(nop);
+    // api format:
+    // [
+    //  {
+    //    Country: "ALA Aland Islands",
+    //    CountryCode: "AX",
+    //    Slug: "ala-aland-islands",
+    //    NewConfirmed: 0,
+    //    TotalConfirmed: 0,
+    //    NewDeaths: 0,
+    //    TotalDeaths: 0,
+    //    NewRecovered: 0,
+    //    TotalRecovered: 0,
+    //    Date: "2020-05-04T18:06:50Z"
+    //  },
+    // ]
+    // svgmap format: 
+    // AF: {
+    //  TotalConfirmed: 0,
+    //  NewDeaths: 0,
+    //  TotalDeaths: 0,
+    //  NewRecovered: 0,
+    //  TotalRecovered: 0
+    // },
 
-//     var tCases = response.Global.TotalConfirmed.toLocaleString();
-//     var nConfirmed = response.Global.NewConfirmed.toLocaleString();
-//     var tDeaths = response.Global.TotalDeaths.toLocaleString();
-//     var tRecovered = response.Global.TotalRecovered.toLocaleString();
-
-//     // $("#cases").html(tCases);
-//     // $("#deaths").html(tDeaths);
-//     // $("#recovered").html(tRecovered);
-//     // $("#new").html(nConfirmed);
-// });
-
-new svgMap({
-
+    // parse country data and construct structure for svgmap
+    var countryObjArray = data.Countries;
+    console.log("First country object:", countryObjArray[0]);
+    console.log("First country:", countryObjArray[0].Country);
+    var mapCountryObj = {};
+    for (i=0; i < countryObjArray.length; i++) {
+      // use country code as key to map obj
+      var thisKey = countryObjArray[i].CountryCode;
+      mapCountryObj[thisKey].TotalConfirmed = countryObjArray[i].TotalConfirmed;
+      . . . do this 5 mo times
+    }
+    // generate map w svgmap
+  new svgMap({
 
   targetElementID: 'svgMap',
   data: {
@@ -54,3 +74,9 @@ new svgMap({
     }
   }
 });
+    //
+    // values: mapCountryObj;
+    // render map
+  }
+}
+
